44cc296360b5568655a8d163c9bc50c591266a87
luoye <luoyefe@gmail.com>
2017-04-24 19:06:57 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -1,2 +1,17 @@
-const inspector = new DomInspector();
+const inspector = new DomInspector({
+	theme: 'dom-inspector-theme-default'
+});
 
+inspector.enable();
+// inspector.disable();
+// inspector.destory();
+
+/*
+inspector.on('click', function() {})
+ */
+
+// inspector.currentTarget;
+
+// inspector.getXPath(inspector.currentTarget);
+// inspector.getCssPath(inspector.currentTarget);
+// inspector.getSelector(inspector.currentTarget);

543335c7e8416a9597eeaf14def1ea78a874294f
luoye <luoyefe@gmail.com>
2017-04-30 20:59:00 +0800

    del temp file

--- test/temp.js
+++
@@ -1,6 +0,0 @@
-function log() {
-	const args = [...arguments].pop();
-	return console.log.call(this, arguments[0], args);
-}
-
-log('Target element is not found. Warning function name:%c getXPath', 'color: #ff5151');
